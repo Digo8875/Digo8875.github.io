867fcac8b86526fea0ffc0543aa43810c1615a8d
DigoPC <rodrigo_piranguinho@hotmail.com>
2020-09-13 01:52:47 -0300

    Ajustando arquivos.

---
+++ content/js/Init.js
@@ -0,0 +1,20 @@
+/* VAR para se trocar a base_url de localhost para a certa na hospedagem 
+* Ex.: 
+* Para Hospedagem:
+*var base_url = '/BawSawApp/public';
+* Para Localhost
+*var base_url = '';
+* A variável aqui fora da function se torna uma variável GLOBAL. Assim, 
+* em todo lugar que precisarmos usar essa base_url ela poderá ser chamada facilmente. 
+* Desde que esse arquivo JS esteja importado antes do outro que a utilizará */
+var base_url = '';
+
+$(document).ready(
+  //inicializa o html adicionando os envetos js especificados abaixo
+	function(){
+
+		alert('ok');
+
+		//######################################################################################################
+	}
+);
